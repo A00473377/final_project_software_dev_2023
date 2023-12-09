95fa2ae0992e2049ada2ddab9307ee3e73bbf800
A00473377 <144714614+A00473377@users.noreply.github.com>
2023-12-09 00:39:38 -0400

    Adding yml file for azure host

--- EVMwithReact/ClientApp/src/components/ConfirmPage.js
+++ EVMwithReact/ClientApp/src/components/ConfirmPage.js
@@ -6,26 +6,30 @@ const ConfirmedPage = (props) => {
     const { firstName, lastName, appointmentDate, appointmentTime } = props;
 
     return (
-        <div className="container">
-            <h1>Your appointment booking is confirmed!</h1>
-            <p>Please note down the details for your convenience.</p>
+        <div className="container mt-5">
+            <div>
+                <h1>YOUR APPOINTMENT BOOKING IS COFIRMED!</h1>
+                <p>Please note down the details for your convenience.</p>
+            </div>
 
-            <div className="row" >
-                <div className="column col-sm-6">
-                    <h3>First Name:</h3>
-                    <p>{firstName}</p>
-                </div>
-                <div className="column col-sm-6">
-                    <h3>Last Name:</h3>
-                    <p>{lastName}</p>
-                </div>
-                <div className="column col-sm-6 name-font">
-                    <h3>Date:</h3>
-                    <p>{appointmentDate}</p>
-                </div>
-                <div className="column col-sm-6">
-                    <h3>Time:</h3>
-                    <p>{appointmentTime}</p>
+            <div>
+                <div className="row">
+                    <div className="col-sm-6 name-font">
+                        <h3>First Name:</h3>
+                        <p>{firstName}</p>
+                    </div>
+                    <div className="col-sm-6 name-font">
+                        <h3>Last Name:</h3>
+                        <p>{lastName}</p>
+                    </div>
+                    <div className="col-sm-6 name-font">
+                        <h3>Date:</h3>
+                        <p>{appointmentDate}</p>
+                    </div>
+                    <div className="col-sm-6 name-font">
+                        <h3>Time:</h3>
+                        <p>{appointmentTime}</p>
+                    </div>
                 </div>
             </div>
         </div>
